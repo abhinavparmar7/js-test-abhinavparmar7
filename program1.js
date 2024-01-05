da3253958a5958bd3e85e897a34b714216d330d9
abhinavparmar7 <107924228+abhinavparmar7@users.noreply.github.com>
2024-01-05 13:52:39 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,31 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+     const left = [];
+      for (let i = 0; i < s.length; i++) 
+      { 
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') 
+        {
+            left.push(s[i]); 
+        } 
+        else if (s[i] === ')' && left.length !== 0 && left[left.length - 1] === '(') 
+        {
+            left.pop(); 
+        } 
+        else if(s[i] === '}' && left.length !== 0 && leftSymbols[left.length - 1] === '{')
+        { 
+            left.pop(); 
+        }
+        else if (s[i] === ']' && left.length !== 0 && left[left.length - 1] === '[')
+        { 
+            left.pop();
+        }
+        else 
+        { 
+            return false; 
+        } 
+    } 
+    return left.length === 0;
 };
 
 module.exports = { isValid };
